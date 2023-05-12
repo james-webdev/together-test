d0a97f7abd78f92cb326c98686b02c1c2d354313
James Roe <58564719+james-webdev@users.noreply.github.com>
2023-05-12 17:50:46 -0500

    add aws image

--- src/components/test/JamesComp.js
+++ src/components/test/JamesComp.js
@@ -1,13 +1,17 @@
 import React from "react";
+import AwsImage from '../../assets/images/aws.js'
 
 
-export function JamesComp({something}) {
-	
+export function JamesComp({ something }) {
+
 	return (
 		<div className="bg-green-300">
 			{something}
 			THIS IS MY COMPY!!! check me outtututututuubowubvowudb
+			<AwsImage/>
 		</div>
+		
+
 	);
 }
 
